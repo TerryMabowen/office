573e39d6c980d01b2f67c8fd54adc85ed9df9228
mabowen <mabw@bell.ai>
2020-07-10 09:45:00 +0800

    fixed

--- office-mybatis-plus-demo/src/main/java/com/mbw/office/demo/web/config/WebMvcConfig.java
+++ office-mybatis-plus-demo/src/main/java/com/mbw/office/demo/web/config/WebMvcConfig.java
@@ -30,6 +30,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**").addResourceLocations("classpath:/templates/");
     }
 
     /**
