7a1b09acd4d4ec016848dfae8a7a2ac95aa0f4b1
mabowen <mabw@bell.ai>
2020-09-30 13:57:04 +0800

    fixed

--- office-wexin-pay-demo/src/main/java/com/mbw/office/demo/biz/service/config/WxPayConfig.java
+++ office-wexin-pay-demo/src/main/java/com/mbw/office/demo/biz/service/config/WxPayConfig.java
@@ -27,18 +27,18 @@ public class WxPayConfig {
 
         //普通商户
 //        WxPayProperty p1 = WxPayProperty.builder()
-//                .appId("wxb128560ae3eedf07")
-//                .mchId("1529287821")
-//                .mchKey("A4659A63476446D29526D8FC78CQBELL")
+//                .appId("wxb128560ae3eedf07mbw")
+//                .mchId("1529287821mbw")
+//                .mchKey("A4659A63476446D29526D8FC78CQBELLmbw")
 //                .isServiceMch(false)
 //                .build();
 //        list.add(p1);
 
         //服务商商户
         WxPayProperty p2 = WxPayProperty.builder()
-                .appId("wx7559b35e16e570ba")
-                .mchId("1600443993")
-                .mchKey("wx7559b35e16e570baHEKONG20200724")
+                .appId("wx7559b35e16e570bambw")
+                .mchId("1600443993mbw")
+                .mchKey("wx7559b35e16e570baHEKONG20200724mbw")
                 .isServiceMch(false)
                 .build();
         list.add(p2);
