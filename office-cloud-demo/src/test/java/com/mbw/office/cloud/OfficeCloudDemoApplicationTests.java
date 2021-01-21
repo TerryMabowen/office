da4d29e9a4699e879034107d7c8ecb5ea26f838d
mabowen <mabw@bell.ai>
2021-01-21 12:01:54 +0800

    fixed

---
+++ office-cloud-demo/src/test/java/com/mbw/office/cloud/OfficeCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbw.office.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
