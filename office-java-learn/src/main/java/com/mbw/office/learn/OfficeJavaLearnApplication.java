38d6042f84d9328fada4ba96ac670d37ff6081bc
mabowen <mabw@bell.ai>
2021-02-03 16:54:16 +0800

    fixed

---
+++ office-java-learn/src/main/java/com/mbw/office/learn/OfficeJavaLearnApplication.java
@@ -0,0 +1,16 @@
+package com.mbw.office.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mabowen
+ */
+@SpringBootApplication
+public class OfficeJavaLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeJavaLearnApplication.class, args);
+    }
+
+}
---
+++ office-java-learn/src/test/java/com/mbw/office/learn/Test1.java
@@ -0,0 +1,10 @@
+package com.mbw.office.learn;
+
+/**
+ * TODO
+ *
+ * @author Mabowen
+ * @date 2021-02-03 16:53
+ */
+public class Test1 {
+}
